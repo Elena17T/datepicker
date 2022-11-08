f202c4631c2e98ec8e687e7e01c6148460a2040a
Elena17T <torgasheva17elena@gmail.com>
2022-11-08 22:08:00 +0100

    created new function

--- index.js
+++ index.js
@@ -1,69 +1,146 @@
 const date = new Date();
 
 const renderCalendar = () => {
-    date.setDate(1);
+  date.setDate(1);
 
-const monthDays = document.querySelector('.days');
+  const monthDays = document.querySelector(".days");
 
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate(); //the last day of current month
 
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate(); //the last day of the previous month
 
+  const firstDayIndex = date.getDay();
 
-const firstDayIndex = date.getDay();
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDay(); // return the index number of the last day from previous month
 
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
+  const nextDays = 7 - lastDayIndex - 1; //count from what day starts week in the next month
+  ("");
+  const daysElem = document.querySelector(".days");
 
+  let day = date.getDate();
+  let month = date.getMonth();
 
-const nextDays = 7 - lastDayIndex - 1;
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
+  const year = date.getFullYear();
 
-const months = [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ];
+  document.querySelector(".date h1").innerHTML =
+    months[date.getMonth()] + " " + year; //show current month and year
+  let days = "";
 
-const year = date.getFullYear();
-
-document.querySelector('.date h1').innerHTML = months[date.getMonth()] + ' ' + year;
-let days = '';
-
-for(let x =firstDayIndex; x>0; x--) {
+  //show days of previous month
+  for (let x = firstDayIndex; x > 0; x--) {
     days += `<div class='prev-date'>${prevLastDay - x + 1}</div>`;
-}
+  }
 
-for(let i = 1; i<=lastDay; i++) {
-    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-        days += `<div class='today'>${i}</div>`;
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class='today'>${i}</div>`;
     } else {
-        days += `<div>${i}</div>`;
+      days += `<div class='curDay'>${i}</div>`;
     }
     monthDays.innerHTML = days;
+  }
+
+  //created days for the next month
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class='next-date'>${j}</div>`;
+  }
+};
+
+function listenToClick() {
+  const elements = document.querySelectorAll(".curDay");
+  for (const el of elements) {
+    el.addEventListener("click", clickHandler);
+  }
+  
 }
 
-for(let j = 1; j <= nextDays; j++) {
-    days +=`<div class='next-date'>${j}</div>`;
-}
+//rewrite
+function clickHandler(event) {
+  console.log(event);
+  const year = date.getFullYear();
+  const pickedDate = new Date(year + "-" + (month + 1) + "-" + (i + 1));
+  setDueDate(pickedDate);
+  console.log(pickedDate);
 }
 
-document.querySelector('.prev').addEventListener('click', () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalendar();
-})
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
 
-document.querySelector('.next').addEventListener('click', () => {
-    date.setMonth(date.getMonth() + 1);
-    renderCalendar();
-})
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
 
 renderCalendar();
+
+const elements = document.getElementsByClassName("container");
+for (i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("mousedown", showDatapicker);
+  elements[i].addEventListener("mouseleave", hideDatapicker);
+}
+
+function showDatapicker() {
+  if (this.children.length > 1) {
+    this.children[1].style.height = "auto";
+    this.children[1].style.opacity = "1";
+    this.children[1].style.overflow = "visible";
+  }
+  listenToClick();
+}
+
+function hideDatapicker() {
+  if (this.children.length > 1) {
+    this.children[1].style.height = "0";
+    this.children[1].style.opacity = "0";
+    this.children[1].style.overflow = "hidden";
+  }
+}
+
+function addZero(d) {
+  return d < 10 ? "0" + d : d;
+}
+function getTime(t) {
+  let Y = t.getFullYear();
+  let M = addZero(t.getMonth() + 1);
+  let D = addZero(t.getDate());
+  // console.log(Y, M, D);
+  return `${Y}-${M}-${D}`;
+}
+const curDate = getTime(new Date());
+
+setDueDate(curDate);
+function setDueDate(dueDate) {
+  document.getElementById("DuedateId").value = dueDate;
+}
